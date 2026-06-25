5958ebc039c0e4886ae79814196c4a577cdfc46b
cbl <cao_bolun@qq.com>
2026-06-25 09:49:02 +0800

    新增防重复提交切面接口

--- framework/src/main/java/com/caobolun/framework/idempotent/IdempotentSubmitAspect.java
+++ framework/src/main/java/com/caobolun/framework/idempotent/IdempotentSubmitAspect.java
@@ -1,11 +1,25 @@
 package com.caobolun.framework.idempotent;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.digest.DigestUtil;
+import com.caobolun.framework.context.UserContext;
+import com.caobolun.framework.exception.ClientException;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 /**
  * 防止用户重复提交表单信息切面控制器
@@ -21,5 +35,74 @@ public final class IdempotentSubmitAspect {
     @Value("${app.eval.enabled:false}")
     private boolean evalEnabled;
 
+    /**
+     * 增强方法
+     */
+    @Around("@annotation(com.caobolun.framework.idempotent.IdempotentSubmit)")
+    public Object idempotentSubmitAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        if(evalEnabled){
+            return joinPoint.proceed();
+        }
+        IdempotentSubmit idempotentSubmit = getIdempotentSubmitAnnotation(joinPoint);
+        // 获取分布式锁标识
+        String lockKey = buildLockKey(joinPoint, idempotentSubmit);
+        RLock lock = redissonClient.getLock(lockKey);
+        // 尝试获取分布式锁, 如果获取失败则抛出异常
+        if (!lock.tryLock()) {
+            throw new ClientException(idempotentSubmit.message());
+        }
+        Object result;
+        try {
+            // 执行标记了防重复提交注解的方法原逻辑
+            result = joinPoint.proceed();
+        } finally {
+            lock.unlock();
+        }
+        return result;
+    }
 
+    /**
+     * @return 返回自定义防重复提交注解
+     */
+    public static IdempotentSubmit getIdempotentSubmitAnnotation(ProceedingJoinPoint joinPoint) throws NoSuchMethodException {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method targetMethod = joinPoint.getTarget().getClass().getDeclaredMethod(methodSignature.getName(), methodSignature.getMethod().getParameterTypes());
+        return targetMethod.getAnnotation(IdempotentSubmit.class);
+    }
+
+    /**
+     * @return 获取当前线程上下文 ServletPath
+     */
+    private String getServletPath() {
+        ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        return Objects.requireNonNull(sra).getRequest().getServletPath();
+    }
+
+    /**
+     * @return 当前操作用户 ID
+     */
+    private String getCurrentUserId() {
+        return UserContext.getUserId();
+    }
+
+    /**
+     * @return joinPoint md5
+     */
+    private String calcArgsMD5(ProceedingJoinPoint joinPoint) {
+        return DigestUtil.md5Hex(gson.toJson(joinPoint.getArgs()).getBytes(StandardCharsets.UTF_8));
+    }
+
+    private String buildLockKey(ProceedingJoinPoint joinPoint, IdempotentSubmit idempotentSubmit) {
+        if (StrUtil.isNotBlank(idempotentSubmit.key())) {
+            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+            Object keyValue = SpELUtil.parseKey(idempotentSubmit.key(), signature.getMethod(), joinPoint.getArgs());
+            return String.format("idempotent-submit:key:%s", keyValue);
+        }
+        return String.format(
+                "idempotent-submit:path:%s:currentUserId:%s:md5:%s",
+                getServletPath(),
+                getCurrentUserId(),
+                calcArgsMD5(joinPoint)
+        );
+    }
 }
